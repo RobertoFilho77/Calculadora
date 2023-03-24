00b826cf4c901a4089f06a69a00a9f82db93efdb
RobertoFilho <robertofilho77@gmail.com>
2023-03-24 14:01:24 -0300

    Initial Commit

---
+++ ParImpar/ParImparApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParImparApp.swift
+//  ParImpar
+//
+//  Created by user on 24/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ParImparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
